10efcb3dae77a3f4e3a5b8f37c30aaeef0884d76
Hazem Safetli <safetli@me.com>
2015-11-28 14:26:11 +0100

    created ConnectionManager class with get function

---
+++ Integreat/Integreat/connectionManger/SlimConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  SlimConnectionManager.h
+//  Integreat
+//
+//  Created by Hazem Safetli on 28/11/15.
+//  Copyright © 2015 Integreat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SlimConnectionManager : NSObject
+
+@end
---
+++ Integreat/Integreat/connectionManger/SlimConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  SlimConnectionManager.m
+//  Integreat
+//
+//  Created by Hazem Safetli on 28/11/15.
+//  Copyright © 2015 Integreat. All rights reserved.
+//
+
+#import "SlimConnectionManager.h"
+
+@implementation SlimConnectionManager
+
+@end
