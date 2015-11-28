5da702bd56d016abea78656df5b12d8f329a4906
Chris Schneider <chrisschneider@me.com>
2015-11-28 12:33:08 +0100

    Project setup

---
+++ Integreat/Integreat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Integreat
+//
+//  Created by Chris Schneider on 28/11/15.
+//  Copyright © 2015 Integreat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
